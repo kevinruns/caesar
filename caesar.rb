cf6551775f93707d2b71d396d91ed0efcbbaf8ac
Kevin Lenehan <klenehan123@gmail.com>
2020-10-30 13:00:40 +0000

    first working version

---
+++ caesar.rb
@@ -0,0 +1,65 @@
+
+
+require 'pry'
+
+$letterToNum = {}
+$numToLetter = {}
+i = 0
+
+alphabet_array = [*'a'..'z']
+
+alphabet_array.each do |lclet|
+  $letterToNum[lclet] = i
+  $numToLetter[i] = lclet
+  i = i + 1
+end
+
+def runCaesar(string, move)
+
+  stringArr = string.split("")
+  newStringArr = []
+
+  stringArr.each do |letter|
+
+    uc = false	
+    newLetter = ""
+      
+    puts letter
+
+    if letter.match(/[^A-Za-z]/)
+      newStringArr.push(letter)
+    
+    else
+    
+      if letter =~ /[A-Z]/ 
+    	  uc = true
+    	  letter = letter.downcase
+      end
+
+      num = $letterToNum[letter]
+      codeNum = (num + move) % 26
+      newLetter = $numToLetter[codeNum]
+
+      if uc then
+        newLetter= newLetter.upcase
+      end
+
+  #    binding.pry
+
+      newStringArr.push(newLetter)
+
+    end  
+  end
+
+  outStr = newStringArr.join("")
+  puts outStr
+end
+
+
+puts "Please enter a string:"
+string  = gets.chomp
+
+runCaesar(string,5)
+
+
+exit
